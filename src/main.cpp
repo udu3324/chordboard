2e168f7eb47b728eb5cf1192d398c0a5bdb4a203
udu3324 <builder7177@gmail.com>
2024-11-16 17:23:02 -0600

    add most of the io and test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,52 @@
 #include <Arduino.h>
 
-const int ledTest = 21;
+const int effectButton = 43;
+const int effectLED[] = {0, 17, 18, 21};
+
+const int engineButton = 46;
+const int engineLED[] = {38, 39, 40, 41};
+
+const int chordButton[] = {9, 11, 13, 15, 10, 12, 14, 16};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  pinMode(ledTest, OUTPUT);
+  Serial.begin(115200);
+
+  pinMode(effectLED[0], OUTPUT);
+  pinMode(effectLED[1], OUTPUT);
+  pinMode(effectLED[2], OUTPUT);
+  pinMode(effectLED[3], OUTPUT);
+
+  pinMode(engineLED[0], OUTPUT);
+  pinMode(engineLED[1], OUTPUT);
+  pinMode(engineLED[2], OUTPUT);
+  pinMode(engineLED[3], OUTPUT);
+
+  pinMode(chordButton[0], INPUT_PULLUP);
+  pinMode(chordButton[1], INPUT_PULLUP);
+  pinMode(chordButton[2], INPUT_PULLUP);
+  pinMode(chordButton[3], INPUT_PULLUP);
+  pinMode(chordButton[4], INPUT_PULLUP);
+  pinMode(chordButton[5], INPUT_PULLUP);
+  pinMode(chordButton[6], INPUT_PULLUP);
+  pinMode(chordButton[7], INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("working!!!");
-  digitalWrite(ledTest, HIGH);
-  delay(500);
-  digitalWrite(ledTest, LOW);
-  delay(500);
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(chordButton[i]) == HIGH) {
+      Serial.println("pressing" + i);
+      analogWrite(effectLED[i], 0);
+    } else {
+      analogWrite(effectLED[i], 1);
+    }
+  }
+
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(chordButton[i + 4]) == HIGH) {
+      Serial.println("pressing" + i);
+      analogWrite(engineLED[i], 0);
+    } else {
+      analogWrite(engineLED[i], 1);
+    }
+  }
 }
